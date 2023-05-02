f680d800a82a326844b1a236ba27f00c0eb98560
lbritors <britoleticiars@gmail.com>
2023-05-02 15:38:41 -0300

    fix: route carrinho

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
-import { deleteProductFromCart, editProductFromCart, listProductFromCart } from "../controllers/products.controllers.js";
+import { deleteProductFromCart, editProductFromCart, listProductsFromCart } from "../controllers/products.controllers.js";
 
 const productsRouter = Router();
 /*transactionsRouter.get("/produtos/:tipo", getProductByType);
 transactionsRouter.post("/carrinho", authValidation, createCartShop);
 transactionsRouter.post("/finalizar-compra", authValidation, createCartShop);*/
 
-// productsRouter.delete("/carrinho/:id", authValidation, deleteProductFromCart);
-// productsRouter.get("/carrinho", authValidation, listProductsFromCart);
-// productsRouter.put("/carrinho:id", authValidation, editProductFromCart)
+productsRouter.delete("/carrinho/:id", authValidation, deleteProductFromCart);
+productsRouter.get("/carrinho", authValidation, listProductsFromCart);
+productsRouter.put("/carrinho:id", authValidation, editProductFromCart);
 
 export default productsRouter;
